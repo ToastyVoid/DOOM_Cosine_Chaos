f9376c67fadabb4668ff046b67d594687c5e56e2
ToastyVoid <bonlonsk@myyahoo.com>
2025-10-22 23:29:42 -0700

    Refactor dothejob.sh for improved build process
    Updated build script for MSYS2 MINGW64 environment.

--- dothejob.sh
+++ dothejob.sh
@@ -1,3 +1,15 @@
-:wqmake distclean 2>/dev/null || true
-./configure --disable-dependency-tracking
-make
+# Uncomment this stuff if you need it
+# ./autogen.sh --host=i686-w64-mingw32
+# Uncomment this thing under ONLY IF you have a configure script
+# ./configure
+
+make clean
+cd pkg/win32
+make clean
+cd ../..
+make -j4
+cd pkg/win32
+make -j4
+cd staging-doom
+
+# BTW this code is designed to be runned in a MSYS2 MINGW64 environment
